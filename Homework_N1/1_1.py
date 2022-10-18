10e6a27a99b47a9a10e3a5df57ef2ca10e5d61c1
Julia0602k <julia0602k@gmail.com>
2022-10-18 12:50:35 +0300

    111

--- Homework_N1/1_1.py
+++ Homework_N1/1_1.py
@@ -1,1 +1,15 @@
 # 5 способов создания строк
+
+str1 = 'Hello'
+str2 = "Can't write"
+str3 = '''Hello
+world'''
+str4 = input('Write a string: ')
+str5 = str(111)
+
+print(str1)
+print(str2)
+print(str3)
+print(str4)
+print(str5)
+
