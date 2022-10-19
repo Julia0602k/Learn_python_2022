732241fc29e1e40f588b19b2479243ee33860dc0
Julia0602k <julia0602k@gmail.com>
2022-10-19 16:34:18 +0300

    111

--- Homework_N2/2_5.py
+++ Homework_N2/2_5.py
@@ -2,3 +2,17 @@
 # Использовать только if, elif, else, and, not, or.
 # Если получилось, то можно попробовать через функцию min()
 # Я про нее еще не рассказывал, но если знаешь ее, то реши.
+
+num1 = int(input('Введите число N1: '))
+num2 = int(input('Введите число N2: '))
+num3 = int(input('Введите число N3: '))
+num4 = int(input('Введите число N4: '))
+
+if num1 < num2 and num1 < num3 and num1 < num4:
+    print('Наименьшее число N1: ', num1)
+elif num2 < num3 and num2 < num4:
+    print('Наименьшее число N2: ', num2)
+elif num3 < num4:
+    print('Наименьшее число N3: ', num3)
+else:
+    print('Наименьшее число N4: ', num4)
