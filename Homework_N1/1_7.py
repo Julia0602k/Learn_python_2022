9914ae15de0acc43bb55ba51d4e9ed72d8b07e8d
Julia0602k <julia0602k@gmail.com>
2022-10-20 06:07:44 +0300

    1111

--- Homework_N1/1_7.py
+++ Homework_N1/1_7.py
@@ -19,13 +19,33 @@
 #     print('Ошибка! Введите четырехзначное число!')
 
 #Variant 2
-num4 = int(input('Введите четырехзначное число: '))
-# if len(num4) == 4:
-if num4 >= 1000 and num4 < 10000 and (int(str(num4[0])) + int(str(num4[3]))) == (int(str(num4[1])) - int(str(num4[2]))):
+# num4 = int(input('Введите четырехзначное число: '))
+# # if len(num4) == 4:
+# if num4 >= 1000 and num4 < 10000 and (int(str(num4[0])) + int(str(num4[3]))) == (int(str(num4[1])) - int(str(num4[2]))):
+#     print('ДА')
+# elif num4 >= 1000 and num4 < 10000 and (int(str(num4[0])) + int(str(num4[3]))) != (int(str(num4[1])) - int(str(num4[2]))):
+#     print('НЕТ')
+# else:
+#     print('Ошибка! Введите четырехзначное число!')
+
+# #Variant 3 (не работает, тк нужна проверка на 4 число, а это доп if, а по заданию я должна сделать на 1 меньше
+# num4 = input('Введите четырехзначное число: ')
+# # if len(num4) == 4:
+# answer1 = int(num4[0]) + int(num4[3])
+# answer2 = int(num4[1]) - int(num4[2])
+# if num4 >= 1000 and num4 < 10000 and answer1 == answer2:
+#     print('ДА')
+# elif num4 >= 1000 and num4 < 10000 and answer1 != answer2:
+#     print('НЕТ')
+# else:
+#     print('Ошибка! Введите четырехзначное число!')
+
+#Variant 4
+num4 = input('Введите четырехзначное число: ')
+if (int(num4[0]) + int(num4[3])) == (int(num4[1]) - int(num4[2])) and int(num4) >= 1000 and int(num4) < 10000:
     print('ДА')
-elif num4 >= 1000 and num4 < 10000 and (int(str(num4[0])) + int(str(num4[3]))) != (int(str(num4[1])) - int(str(num4[2]))):
+elif (int(num4[0]) + int(num4[3])) != (int(num4[1]) - int(num4[2])) and int(num4) >= 1000 and int(num4) < 10000:
     print('НЕТ')
 else:
     print('Ошибка! Введите четырехзначное число!')
-
 
