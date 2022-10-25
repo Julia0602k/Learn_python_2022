f7b82dc3db34853e218234b883db6baa8dae231a
Julia0602k <julia0602k@gmail.com>
2022-10-25 13:43:00 +0300

    25.10

--- Homework_N4/4_5.py
+++ Homework_N4/4_5.py
@@ -4,19 +4,35 @@
 # Например пользователь ввел -5 и 3, на экране вывелось -4, -3, -2, -1.
 # (решить через while)
 #
+
 num1 = int(input('Введите первое число: '))
 num2 = int(input('Введите второе число: '))
-if 0 <= num1 <= num2 or 0 <= num2 <= num1:
-    print('Отрицательных чисел нет')
-elif abs(num1 - num2) <= 1:
+if -1 <= num1 <= num2 or -1 <= num2 <= num1 or abs(num1 - num2) <= 1:
     print('Чисел в заданном промежутке нет')
-elif num2 < num1:
+elif num2 < num1 and num1 - num2 > 1:
     num2 += 1
     while num2 < num1 and num2 < 0:
         print(num2, ', ', sep='', end='')
         num2 += 1
-elif num1 < num2:
+elif num1 < num2 and num2 - num1 > 1:
     num1 += 1
     while num1 < num2 and num1 < 0:
         print(num1, ', ', sep='', end='')
         num1 += 1
+
+# num1 = int(input('Введите первое число: '))
+# num2 = int(input('Введите второе число: '))
+# if 0 <= num1 <= num2 or 0 <= num2 <= num1:
+#     print('Отрицательных чисел нет')
+# elif abs(num1 - num2) <= 1:
+#     print('Чисел в заданном промежутке нет')
+# elif num2 < num1:
+#     num2 += 1
+#     while num2 < num1 and num2 < 0:
+#         print(num2, ', ', sep='', end='')
+#         num2 += 1
+# elif num1 < num2:
+#     num1 += 1
+#     while num1 < num2 and num1 < 0:
+#         print(num1, ', ', sep='', end='')
+#         num1 += 1
