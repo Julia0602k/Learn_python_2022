230d2af02c37c564240ae00be22695ad9b6fb62f
Julia0602k <julia0602k@gmail.com>
2022-10-26 16:36:20 +0300

    25.10

--- Homework_N5/5_1.py
+++ Homework_N5/5_1.py
@@ -15,15 +15,18 @@
 
 a = [4, 6, 'pу', 'tell', 78]
 b = [44, 'hello', 56, 'exept', ['world', 5.7], 3, 6]
+len(a+b)
 
 # Задание 1 (сложить 2 списка)
 list = a + b
 a.extend(b)
-print('Ответ 1: ', list, '\n', a)
+print('Ответ 1: \n', list, '\n', a)
 
 # Задание 2 (Перенести 6-ой элемент на 3 позицию уже сложенных списков)
-list.insert(3, list[6])
-print('Ответ 2: ', list)
+list.insert(3, list[5])
+print('Ответ 2: \n', list)
+list.pop(6)
+print(list)
 
 # Задание 3 (Посчитать сколько раз встречается число 6 в уже сложенных списках)
 print('Ответ 3: число 6 встречается', list.count(6), 'раза')
@@ -36,19 +39,19 @@ print('Ответ 5: 1ый элемент вложенного списка: ', 
 
 # Задание 6. (Посчитать сколько раз в общем всречается 1(единица) в новом списке
 
-list6 = [[1, 5.1], 1, 'hello1world', 51, 'exept', ['orange1', 1.7, 1], 3, 6]
-# print(', '.join(list6[0]))
-count = 0
-for row in list6:
-    for elem in row:
-        elem = str(elem)
-        c = elem.find('1')
-        if c >= 0:
-            count += c
-    row = str(row)
-    d = row.find('1')
-    count += d
-print(count)
+# list6 = [[1, 5.1], 1, 'hello1world', 51, 'exept', ['orange1', 1.7, 1], 3, 6]
+# # print(', '.join(list6[0]))
+# count = 0
+# for row in list6:
+#     for elem in row:
+#         elem = str(elem)
+#         c = elem.find('1')
+#         if c >= 0:
+#             count += c
+#     row = str(row)
+#     d = row.find('1')
+#     count += d
+# print(count)
 
 
 
