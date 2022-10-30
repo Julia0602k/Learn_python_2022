d9543bfebaa14605f4295278d1753d206a99b74c
Julia0602k <julia0602k@gmail.com>
2022-10-30 17:45:56 +0300

    30.10

--- Homework_N6/Exam_6.py
+++ Homework_N6/Exam_6.py
@@ -12,7 +12,7 @@
 #
 # Вывести на экран среднее из чисел кождого кортежа, а также его индекс.
 
-#Программа для генерации кортежей
+# #Программа для генерации кортежей
 import random
 tuple1 = ()
 tuple2 = ()
@@ -22,30 +22,29 @@ while num1 % 2 == 0:
     # print(num1)
 print('Количество элементов кортежа tuple1: ', num1)
 num2 = random.randint(3, 99) #рандомное количество элементов для tuple2
-print(num2)
 while num2 % 2 == 0:
     num2 = random.randint(3, 99) #рандомное количество элементов для tuple1
     # print(num2)
 print('Количество элементов кортежа tuple2: ', num2)
+
 # Генерация 1 кортежа
-for i in range(num1):
+while len(tuple1) < num1:
     a = random.randint(0, 1000)
     if a not in tuple1:
         tuple1 += (a,)
     else:
-        num1 += 1
-print(tuple1)
+        continue
+print('Кортеж tuple1', tuple1)
 # Генерация 2 кортежа
-for i in range(num2):
+while len(tuple2) < num2:
     a = random.randint(0, 1000)
     if a not in tuple2:
         tuple2 += (a,)
     else:
-        num2 += 1
-print(tuple2)
-print(' Количество элементов кортежа tuple1: ', num1, '\n', 'Количество элементов кортежа tuple2: ', num2)
+        continue
+print('Кортеж tuple2', tuple2)
 
-#Сравнение сумм элементов кортежей
+# #Сравнение сумм элементов кортежей
 print('Сумма элементов tuple1: ', sum(tuple1), '\nСумма элементов tuple2: ', sum(tuple2))
 if sum(tuple1) > sum(tuple2):
     print('Сумма элементов кортежа tuple1 больше суммы элементов кортежа tuple2')
@@ -55,26 +54,35 @@ else:
     print('Суммы элементов кортежей tuple1 и tuple2 равны')
 
 # # Индексы минимальных и максимальных элементов кортежей
-# print('Индекс максимального элемента кортежа tuple1: ', tuple1.index(max(tuple1)))
-# print('Индекс минимального элемента кортежа tuple1: ', tuple1.index(min(tuple1)))
-# print('Индекс максимального элемента кортежа tuple2: ', tuple2.index(max(tuple2)))
-# print('Индекс минимального элемента кортежа tuple2: ', tuple2.index(min(tuple2)))
-#
-# # Cреднее из чисел каждого кортежа, а также его индекс
-# print('Среднее число кортежа tuple1: ', tuple1[num1 // 2], 'Индекс числа: ', tuple1.index(tuple1[num1 // 2]))
-# print('Среднее число кортежа tuple2: ', tuple2[num2 // 2], 'Индекс числа: ', tuple2.index(tuple2[num2 // 2]))
+print('Индекс максимального элемента кортежа tuple1: ', tuple1.index(max(tuple1)))
+print('Индекс минимального элемента кортежа tuple1: ', tuple1.index(min(tuple1)))
+print('Индекс максимального элемента кортежа tuple2: ', tuple2.index(max(tuple2)))
+print('Индекс минимального элемента кортежа tuple2: ', tuple2.index(min(tuple2)))
+
+# Cреднее из чисел каждого кортежа, а также его индекс
+# Среднее число для кортежа tuple1:
+list1 = list(tuple1)
+print(list1)
+# list11 = list1.copy()
+# list11 = list1
+# print(list11)
+while len(list1) > 1:
+    list1.remove(max(list1))
+    list1.remove(min(list1))
+print('Среднее число кортежа tuple1: ', list1[0])
+print('Индекс числа: ', tuple1.index(list1[0]))
+
+# # Среднее число для кортежа tuple2:
+# list2 = list(tuple2)
+# print(list2)
+# list22 = list2.copy()
+# # list11 = list1
+# print(list11)
+# while len(list11) > 1:
+#     list11.remove(max(list11))
+#     list11.remove(min(list11))
+# print('Среднее число кортежа tuple1: ', str(list11))
+# print('Индекс числа: ', list1.index(list11[0]))
 
 
 
-
-
-
-# print('Среднее число кортежа tuple1: ', end='')
-# print(tuple1[num1 // 2 + 1]) if num1 % 2 else print(tuple1[num1 // 2])
-# print('Индекс числа: ', end='')
-# print(tuple1.index(tuple1[num1 // 2 + 1])) if num1 % 2 else print(tuple1.index(tuple1[num1 // 2]))
-#
-# print('Среднее число кортежа tuple2: ', end='')
-# print(tuple2[num2 // 2 + 1]) if num1 % 2 else print(tuple2[num2 // 2])
-# print('Индекс числа: ', end='')
-# print(tuple2.index(tuple2[num2 // 2 + 1])) if num1 % 2 else print(tuple2.index(tuple2[num2 // 2]))
