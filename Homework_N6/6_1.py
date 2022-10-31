60d43a01474b6b848c3fc6ea7fa9408bc99d3c18
Julia0602k <julia0602k@gmail.com>
2022-10-31 06:13:22 +0300

    31.10

--- Homework_N6/6_1.py
+++ Homework_N6/6_1.py
@@ -25,10 +25,10 @@ print('Индекс максимального элемента: ', list2.index(
 print('Индекс минимального элемента: ', list2.index(list22[0]))
 
 #Способ 3
-for i in list2:
-    if i < list2[list2.index(i) + 1]:
-
-
+# for i in list2:
+#     if i < list2[list2.index(i) + 1]:
+#
+#
 
 
 # # 3. Создайте кортеж с цифрами от 0 до 9 и посчитайте сумму.
@@ -45,21 +45,31 @@ for i in list2:
 #  'и', 'и', 'и', 'т', 'и')
 # Например: 'Буква "и" встречается 11 раз' (и так с каждой)
 # Решить 2-мя способами через цикл и через метод.
-# long_word = ('т', 'т', 'а', 'и', 'и', 'а', 'и', 'и', 'и', 'т', 'т', 'а', 'и', 'и', 'и', 'и', 'и', 'т', 'и')
+print('\nЗадача N4')
+long_word = ('т', 'т', 'а', 'и', 'и', 'а', 'и', 'и', 'и', 'т', 'т', 'а', 'и', 'и', 'и', 'и', 'и', 'т', 'и')
+
+
+my_list = list(long_word)
+list4 = []
+for i in my_list:
+    if i not in list4:
+        print('Количество', i, '=', my_list.count(i))
+        list4.append(i)
+        # my_list.remove(str(i))
+        # print(my_list)
+
+
+
+
+
+
+# print("\nЗадача 4 \nКортеж N4 (2 способ):")
+# # for i in set(long_word):
+# #     print(i, ' - ', long_word.count(i))
+
 # print("\nЗадача 4 \nКортеж N4 (1 способ): \nбуква 'т':", long_word.count('т'))
 # print("буква 'а':", long_word.count('а'))
 # print("буква 'и':", long_word.count('и'))
-#
-# print("\nЗадача 4 \nКортеж N4 (2 способ):")
-# for i in set(long_word):
-#     print(i, ' - ', long_word.count(i))
-
-# my_list = list(long_word)
-# for i in my_list:
-#   a = my_list.count(i)
-#   print('Количество', i, '=', a)
-#   my_list.remove(i)
-#   continue
 
 
 
