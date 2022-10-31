12be26ecaf991504b90b8824546f9a73e0f3204f
Julia0602k <julia0602k@gmail.com>
2022-10-31 09:11:47 +0300

    31.10

--- Homework_N6/Exam_4.py
+++ Homework_N6/Exam_4.py
@@ -7,12 +7,18 @@
 
 word4 = input('Введите слово: ')
 if word4.isalpha():
-    count = 0
-    for i in word4:
-        if i.islower():
-            print(i)
+    count_low = 0
+    count_up = 0
+    for i in range(0, len(word4)-1):
+        if word4[i].islower() and word4[i + 1].islower():
+            count_low += 1
+        elif word4[i].isupper() and word4[i + 1].isupper():
+            count_up += 1
+    print('В слове', word4, 'содержится', count_low, 'пар (-а) нижнего регистра')
+    print('В слове', word4, 'содержится', count_up, 'пар (-а) верхнего регистра')
+    print('В слове', len(word4), 'букв')
+    #Подсчет гласных и согласных
 
-        # if i.islower
 
 else:
     print('Ошибка! Введите слово, состоящее из букв!')
