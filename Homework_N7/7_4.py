ab91625328924cc3e3ca874c0043fc621d524df1
Julia0602k <julia0602k@gmail.com>
2022-10-31 12:24:57 +0300

    31.10

---
+++ Homework_N7/7_4.py
@@ -0,0 +1,8 @@
+# 4. Данные об email-адресах студентов хранятся в словаре:
+# {'gmail.com': ['alena.semyonova', 'ivan.polekhin', 'marina_abrabova'],
+# 'yandex.ru': ['ekaterina_ivanova', 'glebova_nastya'],
+# 'mail.ru': ['roman.kolosov', 'ilya_gromov', 'masha.yashkina']}
+#
+# Нужно дополнить код таким образом, чтобы он вывел все адреса
+# в алфавитном порядке и в формате имя_пользователя@домен.
+#
