976676d372a1f9ac178fc1f4d44095db0c3aca29
Julia0602k <julia0602k@gmail.com>
2022-11-01 11:51:48 +0300

    01.11

--- Homework_N7/7_4.py
+++ Homework_N7/7_4.py
@@ -6,4 +6,28 @@
 # Нужно дополнить код таким образом, чтобы он вывел все адреса
 # в алфавитном порядке и в формате имя_пользователя@домен.
 #
-l = [0, 0, 0, 0]
+dict5 = {'gmail.com': ['alena.semyonova', 'ivan.polekhin', 'marina_abrabova'],
+         'yandex.ru': ['ekaterina_ivanova', 'glebova_nastya'],
+         'mail.ru': ['roman.kolosov', 'ilya_gromov', 'masha.yashkina']
+}
+#вывод списком
+list4 = []
+for k, v in dict5.items():
+    for i in v:
+        a = i + '@' + k
+        list4.append(a)
+list.sort(list4)
+print(list4)
+
+#вывод отдельно
+list.sort(list4)
+for i in range(len(list4)):
+    print(list4[i])
+
+
+
+# list4 = []
+# for k, v in dict5.items():
+#     a = str(v) + str(k)
+#     list4.append(a)
+# print(list4)
