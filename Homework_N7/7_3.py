c8758010b2d50b91965423bc24086d078cba0295
Julia0602k <julia0602k@gmail.com>
2022-11-01 13:27:27 +0300

    01.11

--- Homework_N7/7_3.py
+++ Homework_N7/7_3.py
@@ -14,23 +14,74 @@ dict3 = {
     10: ('Ф', 'Щ', 'Ъ')
 }
 
+# Вариант с доп списком
 word3 = input('Введите слово из букв русского алфавита: ')
 word3 = word3.upper()
-list3 = list(word3)
-# print(word3, list3)
 list_alph = [] #Новый список из букв русского алфавита
 for v in dict3.values():
     list_alph += v
 count3 = 0
-# print(list_alph)
+count4 = 0
 for i in word3: #Проверка на содержание только букв русского алфавита
-    if i in list_alph:
+    if i not in list_alph:
+        print('Ошибка! Введите слово из букв русского алфавита!')
+        break
+    elif i in list_alph:
         for k, v in dict3.items():
             if i in v:
                 count3 += k
-        # print(count3)
-    else:
-        print('Ошибка! Введите слово из букв русского алфавита!')
-        break
-print('Стоимость введенного слова: ', count3) if count3 > 0 else None
+                count4 += 1
+print('Стоимость введенного слова: ', count3) if count4 == len(word3) else None
 
+
+
+
+
+
+# word3 = input('Введите слово из букв русского алфавита: ')
+# word3 = word3.upper()
+# list_alph = [] #Новый список из букв русского алфавита
+# for v in dict3.values():
+#     list_alph += v
+# count3 = 0
+# # if word3.isalpha():
+# for i in word3: #Проверка на содержание только букв русского алфавита
+#     if i in list_alph:
+#         for k, v in dict3.items():
+#             if i in v:
+#                 count3 += k
+#     else:
+#         print('Ошибка! Введите слово из букв русского алфавита!')
+#         break
+# print('Стоимость введенного слова: ', count3) if count3 > 0 else print('Ошибка! Введите слово из букв русского алфавита!')
+
+
+
+# word3 = input('Введите слово из букв русского алфавита: ')
+# word3 = word3.upper()
+# # list3 = list(word3)
+# # print(word3, list3)
+# # list_alph = [] #Новый список из букв русского алфавита
+# # for v in dict3.values():
+# #     list_alph += v
+# # print(list_alph)
+# count3 = 0
+# for i in word3: #Проверка на содержание только букв русского алфавита
+#     for k, v in dict3.items():
+#         for a in v:
+#             if i in v:
+#                 count3 += k
+# print('Стоимость введенного слова: ', count3) if count3 > 0 else print('Ошибка! Введите слово из букв русского алфавита!')
+#     # if
+#     #     else:
+#     #         print('Ошибка! Введите слово из букв русского алфавита!')
+#     #         break
+#     # for v in dict3.values():
+#     #     if i in v:
+#     #         for k, val in dict3.items():
+#     #             if i in val:
+#     #                 count3 += k
+#     #         print(count3)
+#
+#
+#
