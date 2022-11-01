f857555d2df1004bff76397c3080f2a8b155c5ed
Julia0602k <julia0602k@gmail.com>
2022-11-01 11:59:27 +0300

    01.11

--- Homework_N7/7_5.py
+++ Homework_N7/7_5.py
@@ -1,4 +1,7 @@
 # 5. Напишите программу, которая будет генирировать словарь
 # в котором ключами будут числа от 1 до 5,
 # а значениями будут элементы списка:
-# ['orange', 'apple', 'mango', 'banana', 'fruits']
+
+list5 = ['orange', 'apple', 'mango', 'banana', 'fruits']
+dict5 = {a: list5[a] for a in range(5)}
+print(dict5)
