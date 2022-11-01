9d468a64e66fd2a6fb5fa6d1cd4c38ffe5cb7498
Julia0602k <julia0602k@gmail.com>
2022-11-01 10:51:06 +0300

    01.11

--- Homework_N7/7_1.py
+++ Homework_N7/7_1.py
@@ -4,11 +4,34 @@
 # соответствовать правилам задания ключей в словарях.
 
 
-list1 = list(input('Введите список: '))
-print(list1)
+#Превращает список в словарь
+str1 = input("Введите список, например ['a', 'b', 1]: ")
+print(str1)
+list1 = ((str1.removeprefix('[')).removesuffix(']')).split(', ')
+print(list1, type(list1))
+for i in list1:
+    if i in [' ', '[', ']', ',', ', ', '''"''']:
+            # i == '[' or i == ',' or i == ' ' or i == ']' or i == "'":
+        list1.remove(str(i))
 dict1 = {a: a for a in list1}
 print(dict1)
 
-list2 = [1, 2.0, 'abc']
-dict2 = {a: a for a in list2}
-print(dict2)
+#Еще вариант
+# list1 = input('Введите список (например [a, b, c]): ')
+# print(list1)
+# for i in list1:
+#     if i == '[' or i == ',' or i == ' ':
+#         list1.remove('i')
+# dict1 = {a: a for a in list1}
+# print(dict1)
+
+# #Превращает строку в словарь (строка преобразуется в список, а потом в словарь)
+# list11 = list(input('Введите список: '))
+# print(list11)
+# dict1 = {a: a for a in list11}
+# print(dict1)
+#
+# #Вариант
+# list2 = [1, 2.0, 'abc']
+# dict2 = {a: a for a in list2}
+# print(dict2)
