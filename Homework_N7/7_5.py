9ae7efa45bae383ebb0d5a38520dcab6efeef7f7
Julia0602k <julia0602k@gmail.com>
2022-11-01 12:01:51 +0300

    01.11

--- Homework_N7/7_5.py
+++ Homework_N7/7_5.py
@@ -3,5 +3,9 @@
 # а значениями будут элементы списка:
 
 list5 = ['orange', 'apple', 'mango', 'banana', 'fruits']
-dict5 = {a: list5[a] for a in range(5)}
+dict5 = {(a + 1): list5[a] for a in range(5)}
 print(dict5)
+
+#Вариант2:
+dict55 = {a: list5[a - 1] for a in range(1, 6)}
+print(dict55)
