337814ca0d45ca8490519241ff5791b9fbf3950d
Julia0602k <julia0602k@gmail.com>
2022-11-04 16:53:16 +0300

    04.11

--- Homework_N9/9_1.py
+++ Homework_N9/9_1.py
@@ -1,7 +1,9 @@
 # 1. Ввести в файл 'input.txt' 2 числа в одну строку через пробел.
 # Вывести в файл 'output.txt' их разность.
-# f = open('text.txt', 'w')
 with open('input.txt', 'w') as f:
     f.write('7 3')
-# with open('output.txt', 'w') as f:
-#     f.write('7 3')
+with open('input.txt') as f:
+    str1 = f.readline()
+    # print('Строка из файла: ', str1, '; Разность: ', answer1)
+with open('output.txt', 'w') as f:
+    f.write(str(int(str1[0]) - int(str1[-1])))
