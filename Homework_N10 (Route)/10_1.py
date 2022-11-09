9cbfd674c0e14e56f4325a89df11aa41dc965e15
Julia0602k <julia0602k@gmail.com>
2022-11-09 17:32:42 +0300

    09.11

--- Homework_N10 (Route)/10_1.py
+++ Homework_N10 (Route)/10_1.py
@@ -13,7 +13,6 @@ def season():
     for k, v in dict1.items():
         if a in v:
             return k
-
 dict1 = {'зима': ('1', '2', '12'), 'весна': ('3', '4', '5'), 'лето': ('6', '7', '8'), 'осень': ('9', '10', '11')}
 print(season())
 
