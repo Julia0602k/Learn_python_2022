1b1283528e6b42037fb045afd27aeb776d747edd
Julia0602k <julia0602k@gmail.com>
2022-11-10 01:40:12 +0300

    09.11

--- Homework_N10 (Route)/10_2.py
+++ Homework_N10 (Route)/10_2.py
@@ -11,30 +11,26 @@ def ymn1(a, b):
 def del1(a, b):
     return (a / b)
 start = 1
-while start:
-    num1 = input('Введите первое число: ')
-    while not num1.isdigit():
-        num1 = input('Ошибка! Введите первое число: ')
-    num2 = input('Введите второе число: ')
-    while not num2.isdigit():
-        num2 = input('Ошибка! Введите второе число: ')
-    sign = input('Введите операцию + - * / ')
+while int(start):
+    num1 = float(input('Введите первое число: '))
+    sign = input('Введите + - * / ')
     while not sign in ['+', '-', '*', '/']:
-        sign = input('''Ошибка! Введите операцию + - * / ''')
-    num1 = float(num1)
-    num2 = float(num2)
+        sign = input('''Ошибка! Введите + - * / ''')
+    num2 = float(input('Введите второе число: '))
     if sign == '+':
-        plus1(num1, num2)
+        print(plus1(num1, num2))
     elif sign == '-':
-        minus1(num1, num2)
+        print(minus1(num1, num2))
     elif sign == '*':
-        ymn1(num1, num2)
+        print(ymn1(num1, num2))
     elif sign == '/':
         if num2 == 0:
             print('Ошибка! делить на "0" нельзя!')
         else:
-            del1(num1, num2)
-    start = input('''Если вы хотите выйти, введите 0.\n Если хотите продолжить вычисления, введите 1''')
+            print(del1(num1, num2))
+    start = input('''Если вы хотите выйти, введите 0.\n Если хотите продолжить вычисления, введите 1\n Сделайте выбор: ''')
     while not start.isdigit():
         start = input('Ошибка! Введите цифру! Если вы хотите выйти, введите 0.\n Если хотите продолжить вычисления, введите 1')
+
+
 
