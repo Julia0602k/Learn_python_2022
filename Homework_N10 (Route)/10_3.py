79cb1cff61d6b8e774063d25baeb8e9e962f5834
Julia0602k <julia0602k@gmail.com>
2022-11-09 23:57:16 +0300

    09.11

--- Homework_N10 (Route)/10_3.py
+++ Homework_N10 (Route)/10_3.py
@@ -3,28 +3,75 @@
 # Число – кол-во нечётных цифр.
 # Строка – количество букв.
 # Сделать проверку со всеми этими случаями
-a = [1, 2, 3]
-def type_is(a):
-    b = 0
-    c = 0
-    if a is tuple:
-        for i in str(a):
-            if i.isalpha():
-                b += 1
-    elif a is list:
-        for i in str(a):
-            if i.isalpha():
-                b += 1
-            elif i.isdigit():
-                c += 1
-    elif a is int:
-        for i in str(a):
-            if int(i) % 2:
-                b += 1
-    elif a is str(a):
-        for i in str(a):
-            if i.isalpha():
-                b += 1
-    return b, с
 
-print(type_is(a))
+t = ('Hello world', 123, 'April')
+h = ['Hello world', 123, 'April']
+n = 123
+w = 'Hello world'
+def type_tuple(a):
+    b = 0
+    s = []
+    for i in a:
+        if type(i) is str:
+            for j in i:
+                if j.isalpha():
+                    b += 1
+            s.append(str(b))
+            b = 0
+    return b, s
+def type_list(a):
+    b = 0
+    d = 0
+    for i in str(a):
+        if i.isalpha():
+            b += 1
+        elif i.isdigit():
+            d += 1
+    return b, d
+def type_int(a):
+    b = 0
+    for i in str(a):
+        if int(i) % 2:
+            b += 1
+    return b
+def type_str(a):
+    b = 0
+    for i in str(a):
+        if i.isalpha():
+            b += 1
+    return b
+
+
+
+
+#Вариант, где все в функции
+# def type_is(a):
+#     b = 0
+#     s = 0
+#     d = []
+#     if type(a) is tuple:
+#         for i in a:
+#             if type(i) is str:
+#                 for j in i:
+#                     if j.isalpha():
+#                         b += 1
+#                 d.append(str(b))
+#                 b = 0
+#     elif type(a) is list:
+#         for i in str(a):
+#             if i.isalpha():
+#                 b += 1
+#             elif i.isdigit():
+#                 s += 1
+#     elif type(a) is int:
+#         for i in str(a):
+#             if int(i) % 2:
+#                 b += 1
+#     elif type(a) is str:
+#         for i in str(a):
+#             if i.isalpha():
+#                 b += 1
+#     return b, s, d
+#
+# print(type_is(s))
+
