d53021c7ae2ad2c3bc90d94be502c34bd7c4a4c9
Julia0602k <julia0602k@gmail.com>
2022-11-10 00:24:28 +0300

    09.11

--- Homework_N10 (Route)/10_3_new.py
+++ Homework_N10 (Route)/10_3_new.py
@@ -24,63 +24,35 @@ def type_list(a):
             b += 1
         elif i.isdigit():
             d += 1
-
+    print(f'Количество букв списка {b}, количество цифр списка {d}')
     return b, d
 def type_int(a):
     b = 0
     for i in str(a):
         if int(i) % 2:
             b += 1
-
+    print(f'У числа {a} количество нечетных цифр: {b}')
     return b
 def type_str(a):
     b = 0
     for i in str(a):
         if i.isalpha():
             b += 1
-
+    print(f'Количество букв строки {b}')
     return b
+
 t = ('Hello world', 123, 'April')
 h = ['Hello world', 123, 'April']
 n = 123
 w = 'Hello world'
 
+abc = w      #Здесь нужно ввести букву из переменных выше
 
-
-
-
-
-#Вариант, где все в одной функции
-def type_is(a):
-    b = 0
-    s = 0
-    d = []
-    if type(a) is tuple:
-        for i in a:
-            if type(i) is str:
-                for j in i:
-                    if j.isalpha():
-                        b += 1
-                d.append(str(b))
-                b = 0
-        print(f'Длина слов кортежа: {str(d)[1:-1]}')
-    elif type(a) is list:
-        for i in str(a):
-            if i.isalpha():
-                b += 1
-            elif i.isdigit():
-                s += 1
-        print(f'Количество букв списка {b}, количество цифр списка {s}')
-    elif type(a) is int:
-        for i in str(a):
-            if int(i) % 2:
-                b += 1
-        print(f'У числа {a} количество нечетных цифр: {b}')
-    elif type(a) is str:
-        for i in str(a):
-            if i.isalpha():
-                b += 1
-        print(f'Количество букв строки {b}')
-    return b, s, d
-
-type_is(w)
+if type(abc) is tuple:
+    type_tuple(abc)
+elif type(abc) is list:
+    type_list(abc)
+elif type(abc) is int:
+    type_int(abc)
+elif type(abc) is str:
+    type_str(abc)
