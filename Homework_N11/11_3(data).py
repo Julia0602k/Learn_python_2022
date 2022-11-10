faa5383660dd2a2167f9b3781a2299ee5e2710b5
Julia0602k <julia0602k@gmail.com>
2022-11-10 20:50:18 +0300

    10.11

--- Homework_N11/11_3(data).py
+++ Homework_N11/11_3(data).py
@@ -2,18 +2,23 @@
 #
 # Написать функцию "date", принимающую 3 аргумента — день, месяц, год.
 # Вернуть True, если такая дата есть в нашем календаре, иначе — False.
-
-def date(d, m, y):
+def date(day, month, year):
     for k, v in dict1.items():
-        if (y % 4 == 0 and y % 100 != 0 or y % 400 == 0) and m in v and d == 29:
-            print(True)
-            break
-        elif d in range(1, k + 1) and m in v:
-            print(True)
-        else:
-            print(False)
-d = int(input('введите число: '))
-m = int(input('введите номер месяца: '))
-y = int(input('введите год: '))
+        if month in v:
+            if day in range(1, k + 1):
+                print(True)
+            elif day == 29 and month == 2 and (year % 4 == 0 and year % 100 != 0 or year % 400 == 0):
+                print(True)
+            else:
+                print(False)
+day = int(input('введите число: '))
+month = int(input('введите номер месяца: '))
+year = int(input('введите год: '))
+
 dict1 = {28: (2,), 30: (4, 6, 9, 11), 31: (1, 3, 5, 7, 8, 10, 12)}
-date(d, m, y)
+date(day, month, year)
+
+
+
+
+(y % 4 == 0 and y % 100 != 0 or y % 400 == 0)
