c799b595f559d75f4078c9ed03e82ed4c7e84909
Julia0602k <julia0602k@gmail.com>
2022-11-13 06:08:54 +0300

    12.11

--- Homework_N11/11_1(sum).py
+++ Homework_N11/11_1(sum).py
@@ -4,11 +4,12 @@
 # сумму его цифр.
 def print_digit_sum(num):
     sum = 0
-    for i in num:
+    for i in str(num):
         sum += int(i)
     print(f'Сумма цифр числа {num} равна {sum}')
 
 num = input('Введите число: ')
 while not num.isdigit():
     num = input('Ошибка! Введите число: ')
+num = int(num)
 print_digit_sum(num)
