40d917c4c8cb3aac15e8db62a1d0dd2510bf654a
Julia0602k <julia0602k@gmail.com>
2022-11-18 16:51:03 +0300

    16.11

---
+++ Homework_N12 (klass)/12_2.py
@@ -0,0 +1,41 @@
+# 2. Два метода в классе, один принимает в себя либо строку, либо число.
+#
+# Если передают строку, то смотрим:
+# если произведение гласных и согласных букв меньше или равно
+# длине слова, выводить все гласные, иначе согласные;
+# Если передают число то находим, произведение суммы чётных цифр на длину числа.
+#
+# Длину строки и числа искать во втором методе
+#
+class House:
+    def input1(self):
+        abc = input('Введите строку или число: ')
+        return abc
+    def calculation(self, abc):
+        if abc.isdigit:
+            count1 = len(abc)
+            for i in abc:
+                if i % 2 == 0:
+                    count1 *= int(i)
+            return count1
+        elif not abc.isdigit:
+            abc = abc.lower()
+            str_gl = ''
+            str_sogl = ''
+            count_gl = 0
+            count_sogl = 0
+            for i in abc:
+                if i.isalpha:
+                    if i in ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я']:
+                        str_gl += i
+                        count_gl += 1
+                    else:
+                        str_sogl += i
+                        count_sogl += 1
+            if count_gl * count_sogl <= len(abc):
+                return str_gl
+            else:
+                return str_sogl
+house = House()
+print(house.input1())
+print(house.calculation())
