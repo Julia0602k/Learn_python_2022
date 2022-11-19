e4434ed12d9d9b9d98e266bb1ac3e78f24007c49
Julia0602k <julia0602k@gmail.com>
2022-11-19 17:12:52 +0300

    19.11

--- Homework_N12 (klass)/12_3.py
+++ Homework_N12 (klass)/12_3.py
@@ -1,5 +1,23 @@
 # 3. Исправьте ошибки, правильный результат работы описан в конце
-#
+#class Class1:
+    #
+    # def __init__(self, value):
+    #     self.__var = value
+    #
+    #
+    # def read_set_del(self):
+    #     print('Прочтено')
+    #     return self.__var
+    #
+    #
+    # def read_set_del(self, value):
+    #     self.__var = value
+    #     print('Изменено')
+    #
+    #
+    # def read_set_del(self):
+    #     del self.__var
+    #     print('Удалено')
 class Class1:
     def read_set_del(self):
         print('Прочтено')
