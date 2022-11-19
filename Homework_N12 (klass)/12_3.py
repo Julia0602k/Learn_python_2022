e06dfeb6ba5a44ec74d782be0193b7f3aee8ac2b
Julia0602k <julia0602k@gmail.com>
2022-11-19 08:30:09 +0300

    19.11

--- Homework_N12 (klass)/12_3.py
+++ Homework_N12 (klass)/12_3.py
@@ -1,30 +1,17 @@
-#
 # 3. Исправьте ошибки, правильный результат работы описан в конце
 #
 class Class1:
-
-    def __init__(self, value):
-        self.__var = value
-
-
     def read_set_del(self):
         print('Прочтено')
-        return self.__var
-
-
-    def read_set_del(self, value):
-        self.__var = value
+    def read_set_del1(self):
         print('Изменено')
-
-
-    def read_set_del(self):
-        del self.__var
+    def read_set_del2(self):
         print('Удалено')
 
-c1 = Class1(5)
-c1.read_set_del = 35
-c1.read_set_del
-del c1.read_set_del
+c1 = Class1()
+c1.read_set_del()
+c1.read_set_del1()
+c1.read_set_del2()
 #
 # # Если все ошибки исправлена, то на экран должно выводиться:
 # # Прочтено
