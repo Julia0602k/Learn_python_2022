1e6396bcd76cfd218ab4e37862510e7e67574d59
Julia0602k <julia0602k@gmail.com>
2022-11-19 07:46:29 +0300

    19.11

--- Homework_N12 (klass)/12_1.py
+++ Homework_N12 (klass)/12_1.py
@@ -27,47 +27,45 @@ ex1.number1('Первый')
 ex1._number2('Второй')
 ex1._Num1__number3('Третий')
 
-
-
 # Наследование
-# class Flower:
-#     def __init__(self, color):
-#         self.color = color
-#     def print_info(self):
-#         print(f'Это {self.color} цветок')
-# class Summer_Flower(Flower):
-#     def __init__(self, color, height):
-#         super().__init__(color)
-#         self.height = height
-#     def print_info(self):
-#         print(f'Цветок {self.color}, высотой {self.height}')
-# class Spring_Flower(Flower):
-#     def __init__(self, color, month):
-#         super().__init__(color)
-#         self.month = month
-#     def print_info(self):
-#         print(f'''Цветок {self.color}, цветет в месяце "{self.month}"''')
-# class New_flower(Spring_Flower, Summer_Flower):
-#     def __init__(self, color, month, height, price):
-#         super().__init__(color, month, height)
-#         self.price = price
-#     def print_info2(self):
-#         print(f'Цена этого цветка {self.price}')
-# flower1 = Flower('красный')
-# flower2 = Spring_Flower('белый', 'май')
-# flower3 = Summer_Flower('розовый', '50 см')
-#
-# flower1.print_info()
-# flower2.print_info()
-# flower3.print_info()
-#
-# flower2.color = 'синий'
-# flower2.month = 'март'
-# flower2.print_info()
-#
-# flower4 = New_flower('желтый', 'апрель', '50 см', '10 рублей')
-# flower4.print_info()
-# flower4.print_info2()
+class Flower:
+    def __init__(self, color):
+        self.color = color
+    def print_info(self):
+        print(f'Это {self.color} цветок')
+class Summer_Flower(Flower):
+    def __init__(self, color, height):
+        super().__init__(color)
+        self.height = height
+    def print_info(self):
+        print(f'Цветок {self.color}, высотой {self.height}')
+class Spring_Flower(Flower):
+    def __init__(self, color, month):
+        super().__init__(color)
+        self.month = month
+    def print_info(self):
+        print(f'''Цветок {self.color}, цветет в месяце "{self.month}"''')
+class New_flower(Spring_Flower):
+    def __init__(self, color, month, price):
+        super().__init__(color, month)
+        self.price = price
+    def print_info2(self):
+        print(f'Цена этого цветка {self.price}')
+flower1 = Flower('красный')
+flower2 = Spring_Flower('белый', 'май')
+flower3 = Summer_Flower('розовый', '50 см')
+
+flower1.print_info()
+flower2.print_info()
+flower3.print_info()
+
+flower2.color = 'синий'
+flower2.month = 'март'
+flower2.print_info()
+
+flower4 = New_flower('желтый', 'апрель', '10 рублей')
+flower4.print_info()
+flower4.print_info2()
 
 
 
