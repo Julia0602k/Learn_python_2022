4477a1442f4d29f2082f36a48b28b675d39b922c
Julia0602k <julia0602k@gmail.com>
2022-11-20 08:13:43 +0300

    20.11

--- Homework_N12 (klass)/12_3.py
+++ Homework_N12 (klass)/12_3.py
@@ -24,21 +24,3 @@ del ex.read_set_del
 # # Прочтено
 # # Изменено
 # # Удалено
-
-
-
-
-
-# class Class1:
-#     def read_set_del(self):
-#         print('Прочтено')
-#     def read_set_del1(self):
-#         print('Изменено')
-#     def read_set_del2(self):
-#         print('Удалено')
-#
-# c1 = Class1()
-# c1.read_set_del()
-# c1.read_set_del1()
-# c1.read_set_del2()
-
